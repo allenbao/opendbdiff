6c73a7456524a3985fc545712e0c6bc658ad51a7
octavian.ciubotaru <octavian.ciubotaru@ce479192-a84d-0410-a096-3b91cebe092c>
2008-05-20 22:06:15 +0000

    Now columns are loaded too

--- src/main/java/com/google/opendbdiff/App.java
+++ src/main/java/com/google/opendbdiff/App.java
@@ -12,7 +12,7 @@ public class App {
     public void showTables() throws ClassNotFoundException, SQLException {
         String driverCls = "oracle.jdbc.OracleDriver";
         Class.forName(driverCls);
-        String url = "jdbc:oracle:thin:@//192.168.1.2:1521/TINREAD";
+        String url = "jdbc:oracle:thin:@//80.96.217.45:1521/TINREAD";
         Driver driver = DriverManager.getDriver(url);
 	DriverManager.registerDriver(driver);
         Properties props = new Properties();
@@ -22,6 +22,10 @@ public class App {
         MetadataProvider mp = new OracleMetadataProvider(connection);
         for (Table t : mp.getTables("SIBI")) {
             System.out.println(t);
+            for (Column c : t.getColumns()) {
+                System.out.println("  " + c);
+            }
+            System.out.println();
         }
     }
     
--- src/main/java/com/google/opendbdiff/Column.java
+++ src/main/java/com/google/opendbdiff/Column.java
@@ -8,11 +8,33 @@ package com.google.opendbdiff;
 public class Column {
     
     private String columnName;
+    private String dataType;
+    private String defaultValue;
+    private boolean nullable;
     
-    public Column(String columnName) {
+    public Column(String columnName, String dataType, String defaultValue,
+            boolean nullable) {
+        this.columnName = columnName;
+        this.dataType = dataType;
+        this.defaultValue = defaultValue;
+        this.nullable = nullable;
     }
     
     public String getColumnName() {
         return columnName;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(columnName);
+        builder.append("\t");
+        builder.append(dataType);
+        if (defaultValue != null) {
+            builder.append("\tDEFAULT ");
+            builder.append(defaultValue);
+        }
+        builder.append(nullable ? "\tNULL" : "\tNOT NULL");
+        return builder.toString();
     }
 }
