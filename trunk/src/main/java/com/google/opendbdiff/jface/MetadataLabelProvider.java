4dfa6b914ed892efa5848184f014393bb1ff2612
octavian.ciubotaru <octavian.ciubotaru@ce479192-a84d-0410-a096-3b91cebe092c>
2008-05-21 22:22:08 +0000

    Updated user intf

--- trunk/src/main/java/com/google/opendbdiff/jface/MetadataLabelProvider.java
+++ trunk/src/main/java/com/google/opendbdiff/jface/MetadataLabelProvider.java
@@ -2,7 +2,11 @@ package com.google.opendbdiff.jface;
 
 import com.google.opendbdiff.Column;
 import com.google.opendbdiff.Table;
+import java.util.Random;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
 
 /**
  * 
@@ -11,14 +15,45 @@ import org.eclipse.jface.viewers.LabelProvider;
  */
 public class MetadataLabelProvider extends LabelProvider {
     
+    private static final ImageData IMAGE_ADDED = new ImageData(
+            MetadataLabelProvider.class.getResourceAsStream("added.png"));
+    private static final ImageData IMAGE_MODIFIED = new ImageData(
+            MetadataLabelProvider.class.getResourceAsStream("modified.png"));
+    private static final ImageData IMAGE_REMOVED = new ImageData(
+            MetadataLabelProvider.class.getResourceAsStream("removed.png"));
+    private static final ImageData IMAGE_NOCHANGE = new ImageData(
+            MetadataLabelProvider.class.getResourceAsStream("nochange.png"));
+    
     @Override
-    public String getText(Object obj) {
-        if (obj instanceof Table) {
-            return ((Table) obj).getTableName();
+    public String getText(Object element) {
+        if (element instanceof Table) {
+            return ((Table) element).getTableName();
         } else
-        if (obj instanceof Column) {
-            return ((Column) obj).getColumnName();
+        if (element instanceof Column) {
+            Column c = (Column) element;
+            return c.getColumnName() + ' ' + c.getDataType();
         }
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public Image getImage(Object arg0) {
+        // just test the images
+        Image image = null;
+        switch (new Random().nextInt(4)) {
+        case 0:
+            image = new Image(Display.getCurrent(), IMAGE_ADDED);
+            break;
+        case 1:
+            image = new Image(Display.getCurrent(), IMAGE_MODIFIED);
+            break;
+        case 2:
+            image = new Image(Display.getCurrent(), IMAGE_REMOVED);
+            break;
+        case 3:
+            image = new Image(Display.getCurrent(), IMAGE_NOCHANGE);
+            break;
+        }
+        return image;
     }
 }
