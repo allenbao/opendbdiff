1ca398a8fb993e03762fbe2e88a4234198656ba0
octavian.ciubotaru <octavian.ciubotaru@ce479192-a84d-0410-a096-3b91cebe092c>
2008-05-22 15:39:58 +0000

    Added basic infrastructure for diffs

---
+++ trunk/src/main/java/com/google/opendbdiff/diff/Difference.java
@@ -0,0 +1,13 @@
+package com.google.opendbdiff.diff;
+
+/**
+ * 
+ * @author Octavian Ciubotaru
+ * @date 22 May 2008
+ */
+public enum Difference {
+    ADDED,
+    REMOVED,
+    CHANGED,
+    NO_CHANGE
+}
